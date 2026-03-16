346169d092251e3e63ed7d67603a3c8a7194388b
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-16 15:15:33 +0530

    initial comit

---
+++ api-gateway/src/test/java/com/dhanux/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanux.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
